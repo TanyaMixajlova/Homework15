34ced3c041e06f3dbc2636674558d3105ed81f98
TanyaMixajlova <tanya26reg2013@yandex.ru>
2025-12-28 12:53:56 +0300

    Добавлен класс Order.java

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,23 @@
 public class Product {
+    public int id;
+    public String name;
+    public int price;
+    public String category;
+
+    public Product(int id, String name, int price,String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+    @Override
+    public String toString() {
+        return "Товар[артикул= " + this.id + ", название= " + this.name + ", цена= " + this.price + ", категория= ]" + this.category;
+
+    }
+
 
 }
+
+
+
